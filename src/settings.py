990e75e4e949cdd581d4fb6db8c1cc96c0f28020
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-06-17 22:10:42 +0600

    adding static file settings

--- src/settings.py
+++ src/settings.py
@@ -127,10 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'build/static'
-]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'build/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- src/urls.py
+++ src/urls.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include,re_path
 from django.views.generic import TemplateView
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
 ]
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
